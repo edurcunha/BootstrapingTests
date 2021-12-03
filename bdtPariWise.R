5810cb1d5303aa514312937dfbe5e777a023dd0f
Eduardo Cunha <edurcunha@gmail.com>
2021-12-02 22:25:28 -0300

    Update

---
+++ bdtPariWise.R
@@ -0,0 +1,14 @@
+############################################################################################
+#  bdtPariWise (Bootstraped Difference Test)
+############################################################################################
+
+#  FUNCTION DESCRIPTION: This function provides a bootstrap method for contrasting values 
+#                        between to treatments.
+
+#  ARGUMENTS
+# x: .
+# y: .
+# rand: .
+# two.tail: .
+
+bdtPariWise <- function(x, y, ...) {}
