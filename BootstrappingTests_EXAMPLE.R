2cb2ce1890f6f4e42d350fd10fca29f58a38cd67
Eduardo Cunha <edurcunha@gmail.com>
2021-12-06 16:29:30 -0300

    Update

--- BootstrappingTests_EXAMPLE.R
+++ BootstrappingTests_EXAMPLE.R
@@ -16,17 +16,25 @@
 source('BootstrappingTests.R')
 
 # Create a response variable
-y <- c( rnorm(20, 3, 1), rnorm(20, 6, 1) )   
+y <- c( rnorm(20, 3, 1), rnorm(20, 6, 1), rnorm(20, 12, 1) )   
 
 # Create a categorical variable
-x <- as.factor( rep( LETTERS[1:2], each = 20 ) )
+x <- as.factor( rep( LETTERS[1:3], each = 20 ) )
 
-# Run the bootstrapped difference test (bdt)
-difference.test <- bdt(x, y, rand = 99999, two.tail = TRUE )
-difference.test
+# Run the bootstrapped difference test (bdt) for a pair of groups
+difference.test <- bdt(x[x == "B" | x == "C"], y[x == "A" | x == "B"], 
+                       rand = 9999, two.tail = TRUE )
 
 # Explore the outputs
 difference.test$statistic    # Gives the the mean difference between categories.
 difference.test$p            # Gives the probability of not rejecting the null
                              # hypothesis.
 
+
+# Run the bootstrapped difference test (bdt) for all pairwise combinations
+# between groups
+
+difference.tests <- bdtPairWise(x, y, 
+                       rand = 9999, two.tail = TRUE )
+
+difference.tests
